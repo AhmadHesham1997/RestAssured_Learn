bf3f878097f749ed628b5da7c8ac7db9c0331ace
AhmadHesham1997 <a.hesham2014@gmail.com>
2026-03-26 22:45:05 +0200

    Add MapsPayload class for API request body and refactor MapsRequest to use it

--- src/test/java/Maps/MapsRequest.java
+++ src/test/java/Maps/MapsRequest.java
@@ -1,9 +1,12 @@
 package Maps;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
+import static Payloads.MapsPayload.addPlaceRequestBody;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class MapsRequest {
 
@@ -12,23 +15,20 @@ public class MapsRequest {
         //Add place using post method request
         RestAssured.baseURI = "https://rahulshettyacademy.com/"; //We take the baseuri common alone so we dont have to repeat it in every when method and be used in all of methods
         //Rest Assured works in BDD technique which is given,when,then format
-        given().queryParam("key", "qaclick123")// we get the key and vlaue of our url which is our query params
+        String addPlaceResponse =  given().queryParam("key", "qaclick123")// we get the key and vlaue of our url which is our query params
                 .header("Content-Type", "application/json")// we get the header and value of our url which is our header
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\"shoe park\", \"shop\"],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}")// we get the body of our url which is our payload
+                .body(addPlaceRequestBody())// we get the body of our url which is our payload
                 .when().post("maps/api/place/add/json")// we get the method of our url which is post this where we take an action, we send the end point only
                 .then().assertThat().statusCode(200)// we get the status code of our url which is 200 and we assert it for example
-                .log().all();// we log all the response of our url
+                .assertThat().body("scope", equalTo("APP")) // We assert that key scope in body has a value called APP
+                //.log().all();// we log all the response of our url
+                //.log().body() //we log the body only to see it
+                .extract().response().asString();  // Since we need to assert on a specific value in the response we need to extract the response and convert it from json
+                // into string and store it in a variable
+                System.out.println(addPlaceResponse);
+                //Extract placeID from response using JsonPath class which help me get a value from response
+               JsonPath js = new JsonPath(addPlaceResponse); // we create an object of json path and pass the response in it
+                String placeID = js.getString("place_id"); // we use the getString method to get the value of place_id and store it in a variable
+                System.out.println("PlaceID: " + placeID);
     }
 }
---
+++ src/test/java/Payloads/MapsPayload.java
@@ -0,0 +1,19 @@
+package Payloads;
+//This class is used to store Body plyload of api and send it to the main class thru class object instead of hard coding the body
+public class MapsPayload {
+    public static String addPlaceRequestBody(){
+        return "{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.383494,\n" +
+                "    \"lng\": 33.427362\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Frontline house\",\n" +
+                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                "  \"address\": \"29, side layout, cohen 09\",\n" +
+                "  \"types\": [\"shoe park\", \"shop\"],\n" +
+                "  \"website\": \"http://google.com\",\n" +
+                "  \"language\": \"French-IN\"\n" +
+                "}";
+    }
+}
