5552be12c3be6e479a94a0525eae29a1d8e57083
AhmadHesham1997 <a.hesham2014@gmail.com>
2026-03-27 00:07:24 +0200

    Add address validation after update in testMapApi method

--- src/test/java/Maps/MapsRequest.java
+++ src/test/java/Maps/MapsRequest.java
@@ -7,10 +7,11 @@ import static Payloads.MapsPayload.addPlaceRequestBody;
 import static Payloads.MapsPayload.updatePlaceRequestBody;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.testng.Assert.assertEquals;
 
 public class MapsRequest {
     String newAddress = "10th Street of bartenders"; // We define the new address to be updatedString
-    //We define the variable outside the method to be used in both methods and we can change it easily if we want to update the place with another address
+    //We define the variable outside the method to be used in both methods, and we can change it easily if we want to update the place with another address
     @Test
     public void testMapApi(){
         //Add place using post method request
@@ -39,6 +40,28 @@ public class MapsRequest {
                         .when().put("maps/api/place/update/json")
                         .then().assertThat().statusCode(200)
                         .assertThat().body("msg", equalTo("Address successfully updated")) // We assert on msg in the response body
-                        .log().all(); // So i can see the response i dont need to extract a certain key because the response body already has only 1 key
+                        .log().all(); // So I can see the response i don't need to extract a certain key because the response body already has only 1 key
+
+                // Get place using GET request to validate that the address is updated
+                String getPlaceResponse = given().queryParam("key", "qaclick123")
+                        .queryParam("place_id", placeId) // we pass the placeId in the query param to get the place details
+                        .header("Content-Type", "application/json")
+                        .when().get("maps/api/place/get/json")
+                        .then().assertThat().statusCode(200)
+                        .assertThat().body("address", equalTo("10th Street of bartenders")) //Simple type of assertion to confirm that address is changed
+                        .extract().response().asString(); // we extract the response and convert it to string to be able to assert on a specific value in it
+                System.out.println(getPlaceResponse);
+                JsonPath js1 = new JsonPath(getPlaceResponse); // we create an object of json path and pass the response in it
+                String actualAddress = js1.getString("address"); // we use the getString method to get the value of address and store it in a variable
+                System.out.println("Actual Address: " + actualAddress);
+                //Another way of assertion: Assert that the actual address is equal to the new address we updated using hamcrest which is built in tool with restassured
+                if(actualAddress.equals(newAddress)){
+                    System.out.println("Address updated successfully");
+                }else {System.out.println("Address update failed");
+                }
+
+                //Another way of assertion using TestNG assertEquals
+               assertEquals(actualAddress, newAddress, "Address update failed"); // we assert that the actual address is equal to the new address and we can add a message if the assertion failed
+
     }
 }
